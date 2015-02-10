ab7aead1fff65a097966c6e5036ed7b49dfaeec5
Manoj Sethi <neoms21@gmail.com>
2015-02-10 23:21:36 +0000

    Application settigns module

--- FeelKnitService/Model/ApplicationSetting.cs
+++ FeelKnitService/Model/ApplicationSetting.cs
@@ -1,7 +1,13 @@
-﻿namespace FeelKnitService.Model
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FeelKnitService.Model
 {
     public class ApplicationSetting
     {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
         public bool FeelingsUpdated { get; set; }
     }
 }
---
+++ FeelKnitService/Modules/InfoModule.cs
@@ -0,0 +1,16 @@
+﻿namespace FeelKnitService.Modules
+{
+    public class InfoModule : BaseModule
+    {
+        public InfoModule() : base("/info")
+        {
+            Get["/"] = r => GetApplicationInfo();
+        }
+
+        private dynamic GetApplicationInfo()
+        {
+            var applicationSetting = Context.ApplicationSettings.FindOne();
+            return applicationSetting;
+        }
+    }
+}
